78e2c2f5fe05f5fe87c778dbf7ca5753a912cacc
Prachi Fulchand Kamble <2440161@cognizant.com>
2025-10-21 11:01:14 +0530

    add homepage,aboutus and notification

--- src/app/services/appointment-service.ts
+++ src/app/services/appointment-service.ts
@@ -2,15 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppointmentService {
-  private apiUrl = 'https://localhost:7270/api';
-  constructor(private http:HttpClient){}
-  
-  createAppointments(TimeslotId: string,appointmentDate: string, doctorId: number, personId: number,status:string) {
+  private apiUrl = 'https://localhost:7270/api/Notification/getAllNotifications';
+  constructor(private http: HttpClient) {}
+
+  createAppointments(
+    TimeslotId: string,
+    appointmentDate: string,
+    doctorId: number,
+    personId: number,
+    status: string
+  ) {
     const url = `${this.apiUrl}/createAppointments?TimeSlotId=${TimeslotId}&AppointmentDate=${appointmentDate}&DoctorId=${doctorId}&PatientId=${personId}&Status=${status}`;
     return this.http.post(url, null);
   }
-  
 }
