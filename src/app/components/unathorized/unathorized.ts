0e475e188073478d81ac3a79f865152e87d645fe
Nandini Srinivas Gaddam <2440061@cognizant.com>
2025-10-22 11:33:12 +0530

    Merge pull request #5 from saiharshithac/nandini
    jwt front end

---
+++ src/app/components/unathorized/unathorized.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unathorized',
+  imports: [],
+  template: `<h2>Unauthorized Access</h2>
+             <p>You do not have permission to view this page.</p>`,
+  styleUrl: './unathorized.css'
+})
+export class Unathorized {
+
+}
