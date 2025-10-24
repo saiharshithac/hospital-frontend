0c77d3d6cd970fef894f375293c98dd3fd7e2742
Prachi Fulchand Kamble <2440161@cognizant.com>
2025-10-24 10:37:40 +0530

    change in login,routes,treatmentservice

--- src/app/services/treatment-service.ts
+++ src/app/services/treatment-service.ts
@@ -5,12 +5,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TreatmentService {
-  private apiUrl = 'https://localhost:7270/api/createTreatment';
+  private apiUrl = 'https://localhost:7270/api';
   constructor(private http : HttpClient){}
  
   createTreatment(treatmentData :any){
-      const url = `${this.apiUrl}?DoctorId=${treatmentData.DoctorId}&PatientId=${treatmentData.PatientId}&Dtype=${treatmentData.Dtype}&Description=${treatmentData.Description}&Prescription=${treatmentData.Prescription}&FollowUp=${treatmentData.FollowUp}`;
+      const url = `${this.apiUrl}/createTreatment?DoctorId=${treatmentData.DoctorId}&PatientId=${treatmentData.PatientId}&Dtype=${treatmentData.Dtype}&Description=${treatmentData.Description}&Prescription=${treatmentData.Prescription}&FollowUp=${treatmentData.FollowUp}`;
       return this.http.post(url,null);
     }
+
+    getTreatment(){
+      const url = `${this.apiUrl}/getAllTreatmentDone`;
+      return this.http.get(url);
+    }
+
+    getTreatmentById(treatmentData : any){
+      const url = `${this.apiUrl}/getTreatmentDoneById?PatientId=${treatmentData.PatientId}`;
+      return this.http.get(url);
+    }
 }
- 
